1b781699cab19850a929d457f0d4942ddba5900e
Amira Adediran <amira@Amiras-MBP.ipnxng.net>
2020-08-26 18:00:36 +0100

    finish building out form

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ function App(){
 const styles = StyleSheet.create({
     container:{
       flex: 1, 
-      justifyContent: 'center', 
+      paddingTop: 130,
     },
     text:{
       color: 'darkslateblue', 
